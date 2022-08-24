e4bf263b575b6a08860473e3280cadfb08517b9d
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-24 21:00:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+let drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function findMatching(names,matchName){
+
+return names.filter(name => name.toLowerCase() ===matchName.toLowerCase())
+}
+function fuzzyMatch(names,matchName){
+    return names.filter(name => name.charAt(0)===matchName.charAt(0))
+}
+function matchName(drivers, matchingName){
+return drivers.filter(elem => elem.name === matchingName)
+}
